e992ed93b537504185b7fd8c549003e11fa1ada1
jamesmarc2016 <934396065@qq.com>
2021-07-04 10:17:51 +0800

    Update jd_bean_change.js
    t

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -71,7 +71,7 @@ if ($.isNode()) {
   }
 
   if ($.isNode() && allMessage) {
-    await notify.sendNotify(`${$.name}`, `${allMessage}`, { url: `https://bean.m.jd.com/beanDetail/index.action?resourceValue=bean` })
+    await notify.sendNotify(`${$.name}`, `${allMessage}`)
   }
 })()
     .catch((e) => {
