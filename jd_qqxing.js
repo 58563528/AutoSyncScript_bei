ed884e836a929928e6b2b4705a5bdea3e4a81bfb
jamesmarc2016 <934396065@qq.com>
2021-07-04 21:16:22 +0800

    Update jd_qqxing.js
    d

--- jd_qqxing.js
+++ jd_qqxing.js
@@ -397,7 +397,11 @@ function getUid() {
                 } else {
                     data = JSON.parse(data);
                         if (data.result) {
-                           
+                           if ($.index == 1) {
+                               $.myid = data.data.uid
+                               $.shareuuid = data.data.uid
+                               console.log(`账号1欧洲杯助力码为 ${$.shareuuid}`)
+                           }
                            if(data.data.openCardStatus !=3){
                            console.log("当前未开卡,无法助力和兑换奖励哦")
                            }                           
