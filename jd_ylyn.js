8c7ea43ef0cfe9d38dbf40ed0e8f03ef3544f4ed
townsend <jamesmarc6*>
2021-07-19 17:08:35 +0800

    dd

--- jd_ylyn.js
+++ jd_ylyn.js
@@ -34,96 +34,97 @@ if ($.isNode()) {
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
 message = ""
 $.shareuuid = "9c92844e014a401f89333a3434b82303" //
-$.shareuuid1 = ""
-    !(async () => {
-        if (!cookiesArr[0]) {
-            $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {
-                "open-url": "https://bean.m.jd.com/"
-            });
-            return;
-        }
-        for (let i = 0; i <cookiesArr.length; i++) {
-            cookie = cookiesArr[i];
-            cookie1 = cookiesArr[i];
-            if (cookie) {
-                $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
-                $.index = i + 1;
-                $.cando = true
-                $.quming = true
-                $.cow = ""
-                $.openCard = true
-                $.isLogin = true;
-                $.needhelp = true
-                $.foodNum = 0
-                $.nickName = '';
-                $.drawresult = ""
-                $.exchange =0
-                console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
-                if (!$.isLogin) {
-                    $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {
-                        "open-url": "https://bean.m.jd.com/bean/signIndex.action"
-                    });
-                    if ($.isNode()) {
-                        await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-                    }
-                    continue
+$.shareuuid1 = "009eaf92bd964f16b73f78bede43f2c6" //
+!(async () => {
+    if (!cookiesArr[0]) {
+        $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {
+            "open-url": "https://bean.m.jd.com/"
+        });
+        return;
+    }
+    for (let i = 0; i <cookiesArr.length; i++) {
+        cookie = cookiesArr[i];
+        cookie1 = cookiesArr[i];
+        if (cookie) {
+            $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
+            $.index = i + 1;
+            $.cando = true
+            $.quming = true
+            $.cow = ""
+            $.openCard = true
+            $.isLogin = true;
+            $.needhelp = true
+            $.foodNum = 0
+            $.nickName = '';
+            $.drawresult = ""
+            $.exchange =0
+            console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
+            if (!$.isLogin) {
+                $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {
+                    "open-url": "https://bean.m.jd.com/bean/signIndex.action"
+                });
+                if ($.isNode()) {
+                    await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
                 }
-                await genToken()
-                await getActCk()
-                await getToken2()
-                await getshopid()
-                await getMyPin()
-                await adlog()
-                await getUserInfo()
-                
-                if ($.cando) {
-                    await join(100000000000168,1000013402)
-                    await saveCow()   
-                    await getUid($.shareuuid)
-                    await dotask(0)
-                    await dotask(1)
-                    await dotask(12)
-                    await dotask(13,"ziying")
-                    await dotask(13,"pop")
-                    await dotask(21)
-                    await feedCow()
-                    for (let i = 0; i < $.cs; i++) {
-                     await feedCow()   
-                    }
-                    if($.index < 3){
-                        for (let i = 0; i < $.cj; i++) {
-                         await draw()   
-                        }
-                    }
-                }
-                let shareuuid = $.shareuuid;
-                if ($.cando) {
-                    $.shareuuid = $.shareuuid1;
-                    
-                    await join(100000000000168,1000013402)
-                    await saveCow()   
-                    await getUid($.shareuuid)
-                    await dotask(0)
-                    await dotask(1)
-                    await dotask(12)
-                    await dotask(13,"ziying")
-                    await dotask(13,"pop")
-                    await dotask(21)
-                    await feedCow()
-                    for (let i = 0; i < $.cs; i++) {
-                     await feedCow()   
-                    }
-                    if($.index < 3){
-                        for (let i = 0; i < $.cj; i++) {
-                         await draw()   
-                        }
-                    }
-                }
-                $.shareuuid = shareuuid;
+                continue
             }
-        }
+            await genToken()
+            await getActCk()
+            await getToken2()
+            await getshopid()
+            await getMyPin()
+            await adlog()
+            await getUserInfo()
+            if ($.cando) {
+                await join(100000000000168,1000013402)
+                await saveCow()
+                await getUid($.shareuuid)
+                await dotask(0)
+                await dotask(1)
+                await dotask(12)
+                await dotask(13,"ziying")
+                await dotask(13,"pop")
+                await dotask(21)
+                await feedCow()
+                for (let i = 0; i < $.cs; i++) {
+                    await feedCow()
+                }
 
-    })()
+                if($.index < 3){
+                    for (let i = 0; i < $.cj; i++) {
+                        await draw()
+                    }
+                }
+            }
+            let shareuuid = $.shareuuid;
+            if ($.cando) {
+                $.shareuuid = $.shareuuid1;
+
+                await join(100000000000168,1000013402)
+                await saveCow()
+                await getUid($.shareuuid)
+                await dotask(0)
+                await dotask(1)
+                await dotask(12)
+                await dotask(13,"ziying")
+                await dotask(13,"pop")
+                await dotask(21)
+                await feedCow()
+                for (let i = 0; i < $.cs; i++) {
+                    await feedCow()
+                }
+
+                if($.index < 3){
+                    for (let i = 0; i < $.cj; i++) {
+                        await draw()
+                    }
+                }
+            }
+            $.shareuuid = shareuuid;
+        }
+    }
+
+})()
     .catch((e) => $.logErr(e))
     .finally(() => $.done())
 //获取活动信息
@@ -153,7 +154,7 @@ function genToken() {
                 } else {
                     data = JSON.parse(data);
                     $.isvToken = data['tokenKey']
-                       //console.log($.isvToken)
+                    //console.log($.isvToken)
                 }
             } catch (e) {
                 $.logErr(e, resp)
@@ -187,7 +188,7 @@ function getToken2() {
                 } else {
                     data = JSON.parse(data);
                     $.token2 = data['token']
-                       //console.log($.token2)
+                    //console.log($.token2)
                 }
             } catch (e) {
                 $.logErr(e, resp)
@@ -231,10 +232,10 @@ function getActCk() {
 //获取我的pin
 function getshopid() {
     let config = taskPostUrl("/dz/common/getSimpleActInfoVo", "activityId=dz2103100001340201")
-  
+
     return new Promise(resolve => {
         $.post(config, async (err, resp, data) => {
-             
+
             try {
                 if (err) {
                     console.log(`${JSON.stringify(err)}`)
@@ -243,7 +244,7 @@ function getshopid() {
                     data = JSON.parse(data);
                     if (data.result) {
                         $.shopid = data.data.venderId
-                            //console.log($.shopid)
+                        //console.log($.shopid)
                     }
                 }
             } catch (e) {
@@ -274,7 +275,7 @@ function getMyPin() {
                         $.lz_jdpin_token = resp['headers']['set-cookie'].filter(row => row.indexOf("lz_jdpin_token") !== -1)[0]
                         // console.log(data)
                         console.log(`${$.nickname}`);
-                        
+
                     }
                 }
             } catch (e) {
@@ -318,7 +319,7 @@ function adlog() {
 
 function saveCow() {
     return new Promise(resolve => {
-        
+
         let body = `activityId=dz2103100001340201&actorUuid=${$.shareuuid}&shareUuid=${$.shareuuid}&cowNick=%E6%9F%A0%E6%AA%AC`
         let config = taskPostUrl('/dingzhi/yili/yangniu/saveCow', body)
         //   console.log(config)
@@ -333,7 +334,7 @@ function saveCow() {
                         $.cowNick = data.data.cowNick
                         $.log("取名："+$.cowNick)
                     } else if(data.result == false){
-                        
+
                         $.log(data.errorMessage)
                     }
                 }
@@ -384,26 +385,23 @@ function getUid() {
                     console.log(`${$.name} API请求失败，请检查网路重试`)
                 } else {
                     data = JSON.parse(data);
-                        if (data.result) {
-                           if(data.data.openCard == false){
-                           console.log("当前未开卡,无法助力和兑换奖励哦")
-                           await join(100000000000168,1000013402)
-                          
-                           }
-                           if($.index == 1){
-                                $.shareuuid = data.data.actorUuid
-                                console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}\n好友互助码】${$.shareuuid}\n`);
-                           }
-                           if($.index == 2){
-                                $.shareuuid1 = data.data.actorUuid
-                                console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}\n好友互助码】${$.shareuuid1}\n`);
-                           }
-                           
-                           
-                            
-                            
+                    if (data.result) {
+                        if(data.data.openCard == false){
+                            console.log("当前未开卡,无法助力和兑换奖励哦")
+                            await join(100000000000168,1000013402)
+
                         }
-                      }
+                        if($.index == 1){
+                            $.shareuuid = data.data.actorUuid
+                            console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}\n好友互助码】${$.shareuuid}\n`);
+                        }
+                        if($.index == 2){
+                            $.shareuuid1 = data.data.actorUuid
+                            console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}\n好友互助码】${$.shareuuid1}\n`);
+                        }
+
+                    }
+                }
             } catch (e) {
                 $.logErr(e, resp)
             } finally {
@@ -413,42 +411,42 @@ function getUid() {
     })
 }
 function join(venderId,shopId) {
-  return new Promise(resolve => {
-let joinurl ={
-    url: `https://api.m.jd.com/client.action?appid=jd_shop_member&functionId=bindWithVender&body={"venderId":"${venderId}","shopId":"${shopId}","bindByVerifyCodeFlag":1,"registerExtend":{},"writeChildFlag":0}&client=H5&clientVersion=8.5.6&uuid=88888&jsonp=jsonp_1599410555929_50468`,
-    headers: {
-      'Origin': 'https://api.m.jd.com',
-      'Host': 'api.m.jd.com',
-      'accept': '*/*',
-      'user-agent': 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
-      'accept-language': 'zh-Hans-JP;q=1, en-JP;q=0.9, zh-Hant-TW;q=0.8, ja-JP;q=0.7, en-US;q=0.6',
-      'Referer': `https://api.m.jd.com/client.action?appid=jd_shop_member&functionId=bindWithVender&body={"venderId":"${venderId}","shopId":"${shopId}","bindByVerifyCodeFlag":1,"registerExtend":{},"writeChildFlag":0}&client=H5&clientVersion=8.5.6&uuid=88888&jsonp=jsonp_1599410555929_50468`,
-      'Cookie': cookie1,
-    }
-  }
-    
-    $.get(joinurl, async (err, resp, data) => {
-      try {
+    return new Promise(resolve => {
+        let joinurl ={
+            url: `https://api.m.jd.com/client.action?appid=jd_shop_member&functionId=bindWithVender&body={"venderId":"${venderId}","shopId":"${shopId}","bindByVerifyCodeFlag":1,"registerExtend":{},"writeChildFlag":0}&client=H5&clientVersion=8.5.6&uuid=88888&jsonp=jsonp_1599410555929_50468`,
+            headers: {
+                'Origin': 'https://api.m.jd.com',
+                'Host': 'api.m.jd.com',
+                'accept': '*/*',
+                'user-agent': 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+                'accept-language': 'zh-Hans-JP;q=1, en-JP;q=0.9, zh-Hant-TW;q=0.8, ja-JP;q=0.7, en-US;q=0.6',
+                'Referer': `https://api.m.jd.com/client.action?appid=jd_shop_member&functionId=bindWithVender&body={"venderId":"${venderId}","shopId":"${shopId}","bindByVerifyCodeFlag":1,"registerExtend":{},"writeChildFlag":0}&client=H5&clientVersion=8.5.6&uuid=88888&jsonp=jsonp_1599410555929_50468`,
+                'Cookie': cookie1,
+            }
+        }
 
-        data = data.match(/(\{().+\})/)[1]
-        data = JSON.parse(data);
-          if(data.success == true){
-              $.log(data.message)
-          }else if(data.success == false){
-           $.log(data.message)   
-          } 
+        $.get(joinurl, async (err, resp, data) => {
+            try {
 
-      } catch (e) {
-        $.logErr(e, resp)
-      } finally {
-        resolve(data);
-      }
+                data = data.match(/(\{().+\})/)[1]
+                data = JSON.parse(data);
+                if(data.success == true){
+                    $.log(data.message)
+                }else if(data.success == false){
+                    $.log(data.message)
+                }
+
+            } catch (e) {
+                $.logErr(e, resp)
+            } finally {
+                resolve(data);
+            }
+        })
     })
-  })
 }
 
 function feedCow() {
-   
+
     let config = taskPostUrl("/dingzhi/yili/yangniu/feedCow", `activityId=dz2103100001340201&actorUuid=${$.shareuuid}&pin=${encodeURIComponent($.pin)}`)
     //     console.log(config)
     return new Promise(resolve => {
@@ -482,7 +480,7 @@ function feedCow() {
 
 
 function dotask(taskType, taskValue) {
-   
+
     let config = taskPostUrl("/dingzhi/yili/yangniu/saveTask", `activityId=dz2103100001340201&actorUuid=${$.shareuuid}&pin=${encodeURIComponent($.pin)}&taskType=${taskType}&taskValue=${taskValue}`)
     //     console.log(config)
     return new Promise(resolve => {
@@ -512,7 +510,7 @@ function dotask(taskType, taskValue) {
 }
 
 function draw() {
-    
+
     let config = taskPostUrl("/dingzhi/yili/yangniu/start", `activityId=dz2103100001340201&pin=${encodeURIComponent($.pin)}&actorUuid=${$.shareuuid}`)
     //  console.log(config)
     return new Promise(resolve => {
@@ -569,7 +567,7 @@ function taskPostUrl(url, body) {
             'Host': 'lzdz-isv.isvjcloud.com',
             'Accept': 'application/json',
             'X-Requested-With': 'XMLHttpRequest',
-            
+
 
             'Referer': 'https://lzdz-isv.isvjcloud.com/dingzhi/yili/yangniu/activity/4827909?activityId=dz2103100001340201&shareUuid=b44243656a694b6f94bb30a4a5f2a45d&adsource=ziying&shareuserid4minipg=5Iufa9rY657S3OP3PLSpK07oeVP9kq2pYSH90mYt4m3fwcJlClpxrfmVYaGKuquQkdK3rLBQpEQH9V4tdrrh0w==&shopid=1000013402&lng=114.062604&lat=29.541501&sid=6e9bfee3838075a72533536815d8f3ew&un_area=4_48201_54794_0',
             'user-agent': 'jdapp;android;10.0.4;11;2393039353533623-7383235613364343;network/wifi;model/Redmi K30;addressid/138549750;aid/290955c2782e1c44;oaid/b30cf82cacfa8972;osVer/30;appBuild/88641;partner/xiaomi001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; Redmi K30 Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045537 Mobile Safari/537.36',
